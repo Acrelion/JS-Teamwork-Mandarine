bfa64832e8c6c2fd6632ac8eb1439cb89af1e845
Peter Velkov <kidroca@gmail.com>
2015-07-26 16:10:22 +0300

    Added a RadarChart (to be customized later) Ordered the load-order of scripts - for future modules/scripts: libraries must load fitst first, then modules, then the main.js script. Small changes to the HTML Added a wrapper event listener (if it is not correct or it seems too complex, separate eventListeners may be used for each button)

--- app/scripts/app-modules/radarChart.js
+++ app/scripts/app-modules/radarChart.js
@@ -1,8 +1,43 @@
 var radarChart = (function () {
-	var radarChart, ctx;
+	var ctx,
+		radarChart = {},
+		data = {},
+		options = {};
 
-	ctx = document.getElementById('canvas-chart').getContext('2d');
-	radarChart = new Chart(ctx).Radar(data, options);
+	ctx = document.getElementById('canvas-for-charts').getContext('2d');
+
+	data = {
+	    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+	    datasets: [
+	        {
+	            label: "My First dataset",
+	            fillColor: "rgba(220,220,220,0.2)",
+	            strokeColor: "rgba(220,220,220,1)",
+	            pointColor: "rgba(220,220,220,1)",
+	            pointStrokeColor: "#fff",
+	            pointHighlightFill: "#fff",
+	            pointHighlightStroke: "rgba(220,220,220,1)",
+	            data: [65, 59, 90, 81, 56, 55, 40]
+	        },
+	        {
+	            label: "My Second dataset",
+	            fillColor: "rgba(151,187,205,0.2)",
+	            strokeColor: "rgba(151,187,205,1)",
+	            pointColor: "rgba(151,187,205,1)",
+	            pointStrokeColor: "#fff",
+	            pointHighlightFill: "#fff",
+	            pointHighlightStroke: "rgba(151,187,205,1)",
+	            data: [28, 48, 40, 19, 96, 27, 100]
+	        }
+	    ]
+	};
+
+	radarChart = {
+		create: function() {
+			return new Chart(ctx).Radar(data, options);
+		}
+	};
 
 	return radarChart;
+
 })(); 
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,65 @@
-var main = (function() {
+(function main() {
+// ************************** Declaration Block **********************************
+	var 	tvPieChart,
+		tvLineChart,
+		tvBarChart,
+		tvRadarChart,
+		divWrapper,
+		canvas = document.getElementById('canvas-for-charts');
+// *******************************************************************************		
 
-	var radarChart,
-		canvasChart = document.getElementById('canvas-chart');
+// *************************** Main Magic ****************************************
+	canvas.setAttribute('width', 600);
+	canvas.setAttribute('height', 400);
 
-	canvasChart.setAttribute('width', 600);
-	canvasChart.setAttribute('height', 400);
+	divWrapper = document.getElementById('wrapper');
 
+	tvPieChart = loadFirstApp; // To be editted
+
+	tvLineChart = loadSecondApp; // To be editted
+
+	tvBarChart = loadThirdApp; // To be editted
+
+	tvRadarChart = radarChart;
+// *******************************************************************************
+
+// *************************** Event Listener/s **********************************
+	divWrapper.addEventListener('click', function(evt) {
+		console.log(evt); // For debugging
+		var clickedId = evt.path[1].id;
+
+		switch(clickedId) {
+			case 'pie-chart-li': tvPieChart();
+				break;
+			case 'line-chart-li': tvLineChart();
+				break;
+			case 'bar-chart-li': tvBarChart();
+				break;		
+			case 'radar-chart-li': tvRadarChart.create();
+				break;
+			default: 
+				break;	
+		}
+		
+	});
+// ******************************************************************************
+	
+	// To be editted (move to separate module)
 	function loadFirstApp() {
 		var inner = '<div id="inner-content">' + 'Pie Chart' + '</div>';
 		document.getElementById('content').innerHTML = inner;
 	}
 
+	// To be editted (move to separate module)
 	function loadSecondApp() {
 		var inner2 = '<div id="inner2-content">' + 'Line Chart' + '</div>';
 		document.getElementById('content').innerHTML = inner2;
 	}
 
+	// To be editted (move to separate module)
 	function loadThirdApp() {
 		var inner3 = '<div id="inner3-content">' + 'Bar Chart' + '</div>';
 		document.getElementById('content').innerHTML = inner3;
 	}
+}());
 
-	return {
-		loadFirstApp: loadFirstApp,
-		loadSecondApp: loadSecondApp,
-		loadThirdApp: loadThirdApp
-	};
-
-})();
