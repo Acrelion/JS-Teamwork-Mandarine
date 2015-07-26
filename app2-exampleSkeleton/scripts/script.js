0bcc839ae9e819d2f2cda4c72b50ed90efd92725
Yordanka Petkova <yori_87@mail.bg>
2015-07-26 11:46:46 +0300

    added example skeleton

---
+++ app2-exampleSkeleton/scripts/script.js
@@ -0,0 +1,17 @@
+
+function loadFirstApp() {
+	var inner = '<div id="inner-content">' + 'Pie Chart' + '</div>';
+	document.getElementById('content').innerHTML = inner;
+};
+
+function loadSecondApp() {
+	var inner2 = '<div id="inner2-content">' + 'Line Chart' + '</div>';
+	document.getElementById('content').innerHTML = inner2;
+};
+
+function loadThirdApp() {
+	var inner3 = '<div id="inner3-content">' + 'Bar Chart' + '</div>';
+	document.getElementById('content').innerHTML = inner3;
+};
+
+
