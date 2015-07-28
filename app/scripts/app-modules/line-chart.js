e7031c3782d802353fc9f668d22d725b2b8beaed
snukal <svilen.ukalski@gmail.com>
2015-07-29 00:07:55 +0300

    line-chart.js update options

--- app/scripts/app-modules/line-chart.js
+++ app/scripts/app-modules/line-chart.js
@@ -45,12 +45,55 @@ var lineChart = (function(){
 	];
 
 	options = {
-		animationSteps: 60,
-		animationEasing: animations[29]
+		//Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines : true,
+
+    //String - Colour of the grid lines
+    scaleGridLineColor : "rgba(255,255,255,0.15)",
+
+    //Number - Width of the grid lines
+    scaleGridLineWidth : 3,
+
+    //Boolean - Whether to show horizontal lines (except X axis)
+    scaleShowHorizontalLines: false,
+
+    //Boolean - Whether to show vertical lines (except Y axis)
+    scaleShowVerticalLines: true,
+
+    //Boolean - Whether the line is curved between points
+    bezierCurve : true,
+
+    //Number - Tension of the bezier curve between points
+    bezierCurveTension : 0.5,
+
+    //Boolean - Whether to show a dot for each point
+    pointDot : true,
+
+    //Number - Radius of each point dot in pixels
+    pointDotRadius : 6,
+
+    //Number - Pixel width of point dot stroke
+    pointDotStrokeWidth : 2,
+
+    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+    pointHitDetectionRadius : 20,
+
+    //Boolean - Whether to show a stroke for datasets
+    datasetStroke : true,
+
+    //Number - Pixel width of dataset stroke
+    datasetStrokeWidth : 4,
+
+    //Boolean - Whether to fill the dataset with a colour
+    datasetFill : true,
+
+    //String - A legend template
+    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+		
 	};
 
 	function createChart() {
-		chart = new Chart(context).Line(data);
+		chart = new Chart(context).Line(data, options);
 	}
 	lineChart = {
 		draw: function() {
