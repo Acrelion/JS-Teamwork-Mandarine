dce3e01c69aeba473088220b8876e547897af03c
Yordanka Petkova <yori_87@mail.bg>
2015-07-28 20:52:48 +0300

    fixed displaying of line chart and bar chart, after svg is loaded

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -72,10 +72,14 @@
 				previousChart = tvDoughnutChart;
 				break;
 			case 'line-chart-button':
+				displayNone(svgContainer);
+				displayBlock(canvas);
 				tvLineChart.draw();
 				previousChart = tvLineChart;
 				break;
 			case 'bar-chart-button':
+				displayNone(svgContainer);
+				displayBlock(canvas);
 				tvBarChart.drawBarChart();
 				break;
 			case 'radar-chart-button':
@@ -97,11 +101,11 @@
 				previousChart = tvPieChart;
 				break;
 			case 'polar-chart-button':
-		        	displayNone(svgContainer);
-		        	displayBlock(canvas);
-		        	tvPolarChart.draw();
-		        	previousChart = tvPolarChart;
-		        	break;
+				displayNone(svgContainer);
+				displayBlock(canvas);
+				tvPolarChart.draw();
+				previousChart = tvPolarChart;
+				break;
 			default:
 				break;
 		}
@@ -117,8 +121,18 @@
 				return;
 			}
 			var letter = paper.text(start, startY, text[index]);
-			letter.attr({ "font-size": 34, "font-family": "Arial, Helvetica, sans-serif", "fill": "#4CFF00", "stroke-width": "1", stroke: "#3D5C9D", "font-weight": "bold" });
-			letter.animate({ x: start, y: endY }, 1000);
+			letter.attr({
+				"font-size": 34,
+				"font-family": "Arial, Helvetica, sans-serif",
+				"fill": "#4CFF00",
+				"stroke-width": "1",
+				stroke: "#3D5C9D",
+				"font-weight": "bold"
+			});
+			letter.animate({
+				x: start,
+				y: endY
+			}, 1000);
 			start += 25;
 			index++;
 		}, 700);
@@ -126,12 +140,12 @@
 
 	animateText(appTitle, 190, -20, 40);
 	animateText(teamName, 170, -20, 80);
-	
-	showButton.addEventListener('click', function () {
+
+	showButton.addEventListener('click', function() {
 		$('#movie-form').show();
 	});
-	hideButton.addEventListener('click', function () {
+	hideButton.addEventListener('click', function() {
 		$('#movie-form').hide();
 	});
 	// ******************************************************************************
-} ());
+}());
