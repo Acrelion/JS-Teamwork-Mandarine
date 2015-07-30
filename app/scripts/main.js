e5bd2ce6f500bf648432d9c8f6fb425054878e93
pkeremidchieva <p.keremidchieva@gmail.com>
2015-07-30 11:35:17 +0300

    pyramid - chart added - working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,7 +9,7 @@
 		tvPieChart,
 		tvPolarChart,
 		tvAreaChart,
-		tvPyramidChart,
+                tvPyramidChart,
 		// divWrapper,
 		navigation,
 		previousChart,
@@ -20,9 +20,9 @@
 		hideButton = document.getElementById('hide-movie-form'),
 		canvas = document.getElementById('canvas-for-charts'),
 		svgContainer = document.getElementById('svg-holder'),
-		bubbleContainer = document.getElementById('bubble-holder'),
-		areaContainer = document.getElementById('area-holder'),
-		pyramidContainer = document.getElementById('pyramid-holder');
+	    bubbleContainer = document.getElementById('bubble-holder'),
+	    areaContainer = document.getElementById('area-holder'),
+	    pyramidContainer = document.getElementById('pyramid-holder');
 
 	// *******************************************************************************		
 
@@ -36,7 +36,7 @@
 
 	tvDoughnutChart = doughnutChart;
 	tvPolarChart = polarChart;
-	tvBarChart = barChart;
+	tvBarChart = barChart; // To be edited
 	tvAreaChart = areaChart;
 	tvBubbleChart = bubbleChart;
 	tvLineChart = lineChart;
@@ -70,7 +70,7 @@
 	// *******************************************************************************
 
 	// *************************** Event Listener/s **********************************
-	navigation.addEventListener('click', function(evt) {
+	navigation.addEventListener('click', function (evt) {
 		console.log(evt); // For debugging
 		var clickedId = evt.target.id;
 
@@ -115,62 +115,60 @@
 				break;
 			case 'polar-chart-button':
 				displayNone(svgContainer);
-				displayBlock(canvas);
-				tvPolarChart.draw();
-				previousChart = tvPolarChart;
-				break;
-			case 'area-chart-button':
-				displayNone(canvas);
-				displayBlock(areaContainer);
-				tvAreaChart.draw();
-				previousChart = tvAreaChart;
-				break;
-			case 'pyramid-chart-button':
-				displayNone(canvas);
-				displayBlock(pyramidContainer);
-				tvPyramidChart.draw();
-				previousChart = tvPyramidChart;
-				break;
-			default:
-				break;
+		        displayBlock(canvas);
+		        tvPolarChart.draw();
+		        previousChart = tvPolarChart;
+		        break;
+	        case 'area-chart-button':
+	        	displayNone(canvas);
+	        	displayBlock(areaContainer);
+	        	tvAreaChart.draw();
+	        	previousChart = tvAreaChart;
+	        	break;
+		    case 'pyramid-chart-button':
+		        displayNone(canvas);
+		        tvPyramidChart.draw();
+		        previousChart = tvPyramidChart;
+		        break;
+		    default:
+		        break;
 		}
 
 	});
-
 	function animateText(text, x, stY, endY) {
-		var index = 0;
-		var start = x;
-		var startY = stY;
-		setInterval(function() {
-			if (!text[index]) {
-				return;
-			}
-			var letter = paper.text(start, startY, text[index]);
-			letter.attr({
-				"font-size": 34,
-				"font-family": "Helvetica, Arial, sans-serif",
-				"fill": "#666",
-				"stroke-width": "1",
-				"stroke": "#666",
-				"font-weight": "bold"
-			});
-			letter.animate({
-				x: start,
-				y: endY
-			}, 200);
-			start += 25;
-			index++;
-		}, 300);
+	    var index = 0;
+	    var start = x;
+	    var startY = stY;
+	    setInterval(function () {
+	        if (!text[index]) {
+	            return;
+	        }
+	        var letter = paper.text(start, startY, text[index]);
+	        letter.attr({
+	            "font-size": 34,
+	            "font-family": "Helvetica, Arial, sans-serif",
+	            "fill": "#666",
+	            "stroke-width": "1",
+	            "stroke": "#666",
+	            "font-weight": "bold"
+	        });
+	        letter.animate({
+	            x: start,
+	            y: endY
+	        }, 200);
+	        start += 25;
+	        index++;
+	    }, 300);
 	}
 
 	animateText(appTitle, 175, -20, 40);
 	animateText(teamName, 155, -20, 80);
 
-	showButton.addEventListener('click', function() {
-		$('#movie-form').show();
+	showButton.addEventListener('click', function () {
+	    $('#movie-form').show();
 	});
-	hideButton.addEventListener('click', function() {
-		$('#movie-form').hide();
+	hideButton.addEventListener('click', function () {
+	    $('#movie-form').hide();
 	});
 	// ******************************************************************************
 }());
