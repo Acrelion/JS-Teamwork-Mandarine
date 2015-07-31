9e28e4750b4268adb46abbdee5625fc7d3c89cab
yori-to <yori_87@mail.bg>
2015-07-31 16:47:53 +0300

    changed colors of doughnut-chart

--- app/scripts/app-modules/doughnut-chart.js
+++ app/scripts/app-modules/doughnut-chart.js
@@ -7,7 +7,27 @@ var doughnutChart = (function() {
 		moviesCollection = [],
 		property,
 		mode,
-		chartIsDrawn = false;
+		chartIsDrawn = false,
+		colors = [{
+			color: '#429510',
+			highlight: '#5db925'
+		}, {
+			color: '#9b46d1',
+			highlight: '#ba59f7'
+		}, {
+			color: '#f3d42b',
+			highlight: '#fde562'
+		}, {
+			color: '#0860c2',
+			highlight: '#318aed'
+		}, {
+			color: '#614b48',
+			highlight: '#8c6560'
+		}, {
+			color: '#d7316c',
+			highlight: '#ef548b'
+		}],
+		genreColorCount = 0;
 
 	ctx = document.getElementById('canvas-for-charts').getContext('2d');
 
@@ -40,10 +60,11 @@ var doughnutChart = (function() {
 		for (var prop in mode) {
 			doughnutData.push({
 				value: mode[prop],
-				color: getRandomColor(),
-				highlight: getRandomColor(),
+				color: colors[genreColorCount].color,
+				highlight: colors[genreColorCount].highlight,
 				label: property + ' ' + prop
 			});
+			genreColorCount += 1;
 		}
 
 		return doughnutData;
@@ -53,29 +74,30 @@ var doughnutChart = (function() {
 
 	//====================GET RANDOM COLOR======================
 
-	function getRandomColor() {
-		var letters = '0123456789ABCDEF'.split('');
-		var color = '#';
-		for (var i = 0; i < 6; i++) {
-			color += letters[Math.floor(Math.random() * 16)];
-		}
-		return color;
-	}
+	// function getRandomColor() {
+	// 	var letters = '0123456789ABCDEF'.split('');
+	// 	var color = '#';
+	// 	for (var i = 0; i < 6; i++) {
+	// 		color += letters[Math.floor(Math.random() * 16)];
+	// 	}
+	// 	return color;
+	// }
 
 	//==========================================
 
 	//=====================OPTIONS=====================
 
 	options = {
-			segmentStrokeColor: "#FFF",
-			// segmentShowStroke: false,
-			percentageInnerCutout: 30
-		};
+		segmentStrokeColor: "#FFF",
+		// segmentShowStroke: false,
+		percentageInnerCutout: 30
+	};
 	//==========================================
 
 	//=============CREATE CHART=============================
 
 	function createChart() {
+		genreColorCount = 0;
 		getData();
 		chart = new Chart(ctx).Doughnut(doughnutData,
 			options);
